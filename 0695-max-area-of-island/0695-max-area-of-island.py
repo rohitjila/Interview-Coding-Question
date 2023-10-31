1cd9331b20d4fb7f3bee9b83d97fc3c7924385fd
Rohit <65089210+rohitjila@users.noreply.github.com>
2023-10-31 22:29:00 +0530

    Time: 124 ms (69.83%), Space: 19.8 MB (32.68%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -0,0 +1,27 @@
+import sys
+sys.setrecursionlimit(10**6)
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        r,c= len(grid) , len(grid[0])
+        visit=set()
+        area=0
+        for i in range(r):
+            for j in range(c):
+                if(grid[i][j] == 1):
+                    area=max(area,self.dfs(i,j,grid,r,c,visit))
+        return area
+    
+    def dfs(self,i,j,grid,r,c,visit):
+        if (i < 0 or j < 0 or i == r or j == c 
+              or grid[i][j] == 0 or (i,j) in visit):
+            return 0
+            
+        visit.add((i,j))
+        return (1+self.dfs(i+1,j,grid,r,c,visit)+
+                self.dfs(i-1,j,grid,r,c,visit)+
+                self.dfs(i,j-1,grid,r,c,visit)+
+                self.dfs(i,j+1,grid,r,c,visit))
+        
+        
+        
+        
