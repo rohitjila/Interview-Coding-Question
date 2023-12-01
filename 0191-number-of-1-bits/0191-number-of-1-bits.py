f153064a57ca763eee019c1317a283a20889ff8e
Rohit <65089210+rohitjila@users.noreply.github.com>
2023-12-01 22:34:14 +0530

    Time: 47 ms (8.36%), Space: 16.2 MB (45.60%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -15,6 +15,9 @@ class Solution:
             count+=1
         return count
     
+#Let num be 25 (binary: 11001). In the first iteration, we flip the rightmost set bit (position 0), and count becomes 1. In the next iteration, we flip the next rightmost set bit (position 3), and count becomes 2. We continue until there are no more set bits, and the final count is 3. So, there are 3 set bits in the binary representation of 25.This leads to time complexity O(k) k is representing  where K is the number of set bits in the binary representation of the input number.
+
+    
     
     
     
