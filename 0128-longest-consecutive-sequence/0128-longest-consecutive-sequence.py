e20322edd04c53b052744161135fe0d508b5f7d2
Rohit <65089210+rohitjila@users.noreply.github.com>
2024-01-14 08:54:51 +0530

    Time: 371 ms (79.28%), Space: 31.7 MB (60.36%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -0,0 +1,97 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        n = len(nums)
+        if(n == 0): return 0
+        #brute force
+        maxCount = 1
+        count = 1
+        nums.sort()
+        for i in range(n-1):
+            if(nums[i] != nums[i+1]):
+                if(nums[i]+1 == nums[i+1]):
+                    count +=1
+                else:
+                    count = 1
+                maxCount = max(count,maxCount)
+        return maxCount
+                    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # [100,4,200,1,3,2]
+        # 1,2,3,4
+        # ans= 4
+        # brute better
+        # longest streak
+        # set={100,4,200,1,3,2}
+        # [100,4,200,1,3,2]
+        # 100 = 1
+        # 200 = 1
+        # 1 ,2 ,3,4 = 4
+        
+        # s = set(nums)
+        # mx=0
+        # for n in nums:
+        #     if n-1 not in s:
+        #         length=0
+        #         while(n + length in s):
+        #             length+=1
+        #         mx = max(length,mx)
+        # return mx
+                
+        
+        
+        
+        #brute force 
+        # 1,2,3,4,100,200
+        # maintain longest()
+        # 4
+#         n = len(nums)
+#         if (n == 0):
+#             return 0
+        
+#         nums.sort()
+#         cs = 1
+#         ls = 1
+#         for i in range(n-1):
+#             if (nums[i] != nums[i+1]):
+#                 if (nums[i]+1 == nums[i+1]):
+#                     cs+=1
+#                 else:
+#                     cs = 1
+#                 ls = max(cs,ls)
+#         return ls
+                
+            
+        
+#             [0,1,1,2]
+        
+        
+        
