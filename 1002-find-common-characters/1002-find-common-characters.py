f9dabeaeaacd50bdc4eb4425e23da76e1406818a
Rohit <65089210+rohitjila@users.noreply.github.com>
2024-01-14 06:56:44 +0530

    Time: 39 ms (97.46%), Space: 17.5 MB (5.22%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.py
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -1,18 +1,11 @@
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        common_chars = []
-    
-        # Iterate over each character in the first word
+        common_char = []
         for char in words[0]:
-            # Check if the character is present in all other words
             if all(char in word for word in words[1:]):
-                # If yes, add it to the common_chars list
-                common_chars.append(char)
-
-                # Remove the first occurrence of the character from all words
-                words = [word.replace(char, '', 1) for word in words]
-
-        return common_chars
+                common_char.append(char)
+                words = [word.replace(char,'',1) for word in words]
+        return common_char
 
         
         
