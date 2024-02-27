ac6cd688d921756a2b08e93d6b51869a49e8e6c9
Rohit <65089210+rohitjila@users.noreply.github.com>
2024-02-27 21:29:59 +0530

    Time: 55 ms (6.63%), Space: 16.6 MB (65.87%) - LeetHub

--- 3046-split-the-array/3046-split-the-array.py
+++ 3046-split-the-array/3046-split-the-array.py
@@ -1,8 +1,26 @@
+from collections import defaultdict
 class Solution:
     def isPossibleToSplit(self, nums: List[int]) -> bool:
         n = len(nums)
-        nums.sort()
-        for i in range(2,n):
-            if(nums[i] == nums[i-2]): return False
+        # nums.sort()
+        # for i in range(2,n):
+        #     if(nums[i] == nums[i-2]): return False
+        # return True
+    
+        #the basic idea is to check if the frequency is more than 2 it means we can't take distinct value
+        #in both the array
+        
+        #brute force
+        #t.c -> O(nlog(n) + O(n))
+        #s.c -> O(1)
+        
+        #brute better
+        
+        mp = defaultdict(lambda : 0)
+        for i  in range(n):
+            mp[nums[i]] += 1
+        for i in mp:
+            if(mp[i] > 2): return False
         return True
+    
         
