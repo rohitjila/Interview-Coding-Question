35bf79c75cf842a32c91ea3a5cec600f32af5a2d
Rohit <65089210+rohitjila@users.noreply.github.com>
2024-02-29 19:58:51 +0530

    Time: 718 ms (93.98%), Space: 28.3 MB (88.83%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.py
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.py
@@ -1,6 +1,7 @@
 #Isme kar kya rahe hai ki sabse pehle hmlog set me har ek number ka prefix daal rahe hai jaise ki 
 #maan lo 123 hai to uska prefix ho jayega 1 12 123 ye set me hai abb hmlog arr2 me jayenge aur check
-#karenge ki wo prefix present hai ki nahi set me 
+#karenge ki wo prefix present hai ki nahi set me har ek number ka iss tarah se koi bhi number
+#ka mil prefix mil jata uss number ka kaam khatam kar dete.
 class Solution:
     def length(self,n):
         ans = str(n)
