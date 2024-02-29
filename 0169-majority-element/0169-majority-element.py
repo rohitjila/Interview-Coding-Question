792a799d10746c3f7a0195731c5cf35642916df1
Rohit <65089210+rohitjila@users.noreply.github.com>
2024-02-29 17:29:43 +0530

    Time: 173 ms (13.11%), Space: 18.1 MB (47.26%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,9 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        #brute force method is just count each number and check whether the number is greate than
+        #brute force method is just count each number and check whether the number is greater than
         #n // 2 if yes the return the major element
+        #we can do this by hashing 
+        #if there is 100 percent chance that majority element exist then simply sort the array and return 
+        #n // 2 index value because till that position majority element will be occupied.
         
-        #brute better
+        #brute better(Moore voting algorithm)
         expectedMajorityElement = nums[0]
         count = 0
         n = len(nums)
