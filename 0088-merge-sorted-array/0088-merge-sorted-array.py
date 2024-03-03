4ab4c5058a4a1fc163c9fbe60f902b3eee989b96
Rohit <65089210+rohitjila@users.noreply.github.com>
2024-03-04 02:50:07 +0530

    Time: 39 ms (61.93%), Space: 16.7 MB (46.13%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,5 +1,53 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        
+        while(m and n):
+            if(nums2[n-1] > nums1[m-1]):
+                nums1[m+n-1] = nums2[n-1]
+                n -=1
+            else:
+                nums1[m+n-1] = nums1[m-1]
+                m -=1
+                
+        while(n):
+            nums1[m+n-1] = nums2[n-1]
+            n -=1
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         #brute force yaha pe kuch nahi kar rahe hai bas second array ka element first element me
         #lagane ke baad sort kar de rahe hai
         #time complexity -> O(m+n) * log(m+n)
@@ -13,17 +61,17 @@ class Solution:
         #arrange karte aana hai
         #T.C : O(m + n)
         #S.C : O(1)
-        while(m > 0 and n > 0):
-            if(nums1[m-1] > nums2[n-1]):
-                nums1[m+n-1] = nums1[m-1]
-                m-=1
-            else:
-                nums1[m+n-1] = nums2[n-1]
-                n-=1
-        #iska yahi mltb hai ki jab m=0 hai to saara value n ka m me append kar dena hai.   
-        while(n > 0):
-            nums1[n-1] = nums2[n-1]
-            n-=1
+        # while(m > 0 and n > 0):
+        #     if(nums1[m-1] > nums2[n-1]):
+        #         nums1[m+n-1] = nums1[m-1]
+        #         m-=1
+        #     else:
+        #         nums1[m+n-1] = nums2[n-1]
+        #         n-=1
+        # #iska yahi mltb hai ki jab m=0 hai to saara value n ka m me append kar dena hai.   
+        # while(n > 0):
+        #     nums1[n-1] = nums2[n-1]
+        #     n-=1
     
             
             
