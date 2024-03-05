10a3dca8f2febd581b448ac503546771d026ff29
Rohit <65089210+rohitjila@users.noreply.github.com>
2024-03-06 00:46:00 +0530

    Time: 55 ms (37.53%), Space: 16.6 MB (44.47%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -1,14 +1,15 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        i = 0
+        i = 0 
         for n in nums:
             if(i < 2 or n > nums[i-2]):
                 nums[i] = n
                 i+=1
         return i
     
+       
 #kya samjha mai iss problem se length agar 2 rahega to just usko return karna ya duplicate ho ya distinct ho 
-#uske baad agar 2 se jyada duplicate value hoga aur hmlog value equal to nahi milega tab greater milega
+#uske baad agar 2 se jyada duplicate value hoga aur hmlog ko value equal to nahi milega tab greater milega
 #to uss position wo greater waala number replace ho jayega aur agar bada nahi hai mtlb i-2 duplicate 
 #value hai to i wohi ruk jayega kykuki wohi replace karna hai aur jaise hi greater mila hmlog usko
 #replace kar diye. 
