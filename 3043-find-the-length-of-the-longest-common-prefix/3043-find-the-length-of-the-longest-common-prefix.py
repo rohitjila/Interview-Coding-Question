b433a64d91af367a23ad671cda8ea959da4b50b0
Rohit <65089210+rohitjila@users.noreply.github.com>
2024-03-11 16:03:26 +0530

    Time: 713 ms (93.91%), Space: 28.4 MB (80.01%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.py
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.py
@@ -3,25 +3,32 @@
 #karenge ki wo prefix present hai ki nahi set me har ek number ka iss tarah se koi bhi number
 #ka mil prefix mil jata uss number ka kaam khatam kar dete.
 class Solution:
-    def length(self,n):
-        ans = str(n)
-        return len(ans)
+    def length(self,m):
+        m = str(m)
+        return len(m)
     
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
-        maxi = 0
-        s =  set()
+        s = set()
+        ans = 0
         for n in arr1:
             while(n > 0):
                 s.add(n)
-                n //=10
+                n = n // 10
                 
         for m in arr2:
             while(m > 0):
                 if(m in s):
-                    maxi = max(maxi,self.length(m))
+                    ans = max(ans,self.length(m))
                     break
-                m //=10
-        return maxi
+                m = m // 10
+        return ans
+                
+        
+        
+        
+        
+        
+
             
                 
         
