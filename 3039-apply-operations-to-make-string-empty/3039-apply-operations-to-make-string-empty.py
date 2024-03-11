d8c27f0cd79e5bd1b4e4eab3412d6fe3578c562c
Rohit <65089210+rohitjila@users.noreply.github.com>
2024-03-11 23:32:22 +0530

    Time: 344 ms (74.13%), Space: 22.5 MB (16.97%) - LeetHub

--- 3039-apply-operations-to-make-string-empty/3039-apply-operations-to-make-string-empty.py
+++ 3039-apply-operations-to-make-string-empty/3039-apply-operations-to-make-string-empty.py
@@ -1,10 +1,10 @@
 class Solution:
     def lastNonEmptyString(self, s: str) -> str:
         count = Counter(s)
-        c = max(count.values())
+        maxCount = max(count.values())
         res = []
         for i in s[::-1]:
-            if(count[i] == c):
+            if(count[i] == maxCount):
                 res.append(i)
                 count[i] -=1
         return "".join(res[::-1])
