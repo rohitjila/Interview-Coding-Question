82ffcc8a0d24478fb025ce810f871e56795052e3
Rohit <65089210+rohitjila@users.noreply.github.com>
2024-03-13 18:56:59 +0530

    Time: 78 ms (5.28%), Space: 20.1 MB (5.07%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,19 +1,30 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        dp = [[-1 for i in range(2)] for i in range(n)]
-        return self.f(0,1,prices,n,dp)
-    
-    def f(self,ind,buy,prices,n,dp):
-        if(ind == n):
-            return 0
-        if(dp[ind][buy] != -1):
-            return dp[ind][buy]
-        if(buy):
-            profit = max((-prices[ind] + self.f(ind+1,0,prices,n,dp)) , self.f(ind+1,1,prices,n,dp))
-        else:
-            profit = max((prices[ind] + self.f(ind+1,1,prices,n,dp)) , self.f(ind+1,0,prices,n,dp))
-        dp[ind][buy] = profit
+        dp = [[0 for i in range(2)] for i in range(n+1)]
+        #base case
+        
+        for ind in range(n-1,-1,-1):
+            for buy in range(0,2):
+                if(buy):
+                    profit = max(-prices[ind] + dp[ind+1][0],dp[ind+1][1])
+                else:
+                    profit = max(prices[ind] + dp[ind+1][1], dp[ind+1][0])
+                dp[ind][buy] =  profit
         return dp[ind][buy]
+                
+        # return self.f(0,1,prices,n,dp)
+    
+    # def f(self,ind,buy,prices,n,dp):
+    #     if(ind == n):
+    #         return 0
+    #     if(dp[ind][buy] != -1):
+    #         return dp[ind][buy]
+    #     if(buy):
+    #         profit = max((-prices[ind] + self.f(ind+1,0,prices,n,dp)) , self.f(ind+1,1,prices,n,dp))
+    #     else:
+    #         profit = max((prices[ind] + self.f(ind+1,1,prices,n,dp)) , self.f(ind+1,0,prices,n,dp))
+    #     dp[ind][buy] = profit
+    #     return dp[ind][buy]
             
             
