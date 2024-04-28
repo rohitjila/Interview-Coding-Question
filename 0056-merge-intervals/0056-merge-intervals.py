0ca1e25045f1c4a72fda4b9439dc104aa2783240
Rohit <65089210+rohitjila@users.noreply.github.com>
2024-04-28 13:54:01 +0530

    Time: 119 ms (78.45%), Space: 20.4 MB (95.68%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -4,8 +4,11 @@ class Solution:
         intervals.sort()
         for interval in intervals:
             if not res or res[-1][1] < interval[0]:
-                res.append(interval)
+                res.append(interval)  #non-overlapping conditions
             else:
-                res[-1][1]= max(res[-1][1], interval[1])
+                #overlapping conditions
+                res[-1][1] = max(res[-1][1] , interval[1])
         return res
-        
+                
+            
+            
